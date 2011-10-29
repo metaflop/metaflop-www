ba104f189de26b25c88f08b1c7c83a4b81807a95
koffeinfrei <mail@koffeinfrei.org>
2011-10-29 23:45:40 +0200

    handle increase/decrease of input values

--- client/assets/application.js
+++ client/assets/application.js
@@ -1,19 +1,41 @@
 $(function () {
 
     // set background to corresponding inputs
-    var setActiveInputs = function(element) {
-        var suffix = element.id.remove(/^\w+-/);
+    var setActiveInputs = function(inputField) {
+        var suffix = (inputField.id || inputField[0].id).remove(/^\w+-/);
         $('.adjuster input').removeClass('active');
         $('input[id$=' + suffix + ']').addClass('active');
+    }
+    
+    // increase/decrease the inputField's value
+    var changeValue = function(inputField, cssClass){
+        var number = cssClass.remove(/\D/g).toNumber();
+        var method = cssClass.remove(/\d+$/);
+        
+        var value = inputField.val().toNumber() || 0;
+        
+        if (method == 'add') value = value + number;
+        else if (method == 'sub') value = value - number;
+        
+        value = value + '%';
+        inputField.val(value);
+        
+        // the linked input too
+        var suffix = inputField[0].id.remove(/^\w+-/);
+        $('input[id$=' + suffix + ']').val(value);
     }
 
     $('.adjuster input').focus(function() {
         setActiveInputs(this);
     });
     
-    $('.add1, .add2, .sub1, .sub2').click(function() {
-        var element = $(this).parent().find('input');
-        setActiveInputs(element[0]);
+    $('.add1, .add10, .sub1, .sub10').click(function() {
+        $this = $(this);
+        var input = $this.parent().find('input');
+        
+        setActiveInputs(input);
+        
+        changeValue(input, $this.attr('class'));
     });
 
 });

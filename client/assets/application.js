eaa99ae735289877085a33fe68828d17d9539a68
koffeinfrei <mail@koffeinfrei.org>
2011-10-29 22:04:08 +0200

    colorize active corresponding input fields

--- client/assets/application.js
+++ client/assets/application.js
@@ -1,5 +1,19 @@
 $(function () {
 
+    // set background to corresponding inputs
+    var setActiveInputs = function(element) {
+        var suffix = element.id.remove(/^\w+-/);
+        $('.adjuster input').removeClass('active');
+        $('input[id$=' + suffix + ']').addClass('active');
+    }
 
+    $('.adjuster input').focus(function() {
+        setActiveInputs(this);
+    });
+    
+    $('.add1, .add2, .sub1, .sub2').click(function() {
+        var element = $(this).parent().find('input');
+        setActiveInputs(element[0]);
+    });
 
 });

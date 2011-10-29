f50656bcaf9f3ef2988f5c0301724c5372b40e6d
koffeinfrei <mail@koffeinfrei.org>
2011-10-29 16:38:09 +0200

    * added parameter panel templates * simplified templating data structure

--- client/assets/init.js
+++ client/assets/init.js
@@ -1,16 +1,55 @@
 $(function () {
 
-    var livePanelAdjuster = {
-        items: [
-            {class: 'body-height', title: 'body height'}
-        ]
+    var params = {
+        capitalHeight:  { name: 'capital-height', title: 'capital height' },
+        xHeight:  { name: 'x-height', title: 'x height' },
+        barHeight:  { name: 'bar-height', title: 'bar height' },
+        ascHeight:  { name: 'asc-height', title: 'ascender height' },
+        descHeight:  { name: 'desc-height', title: 'descender height' },
+        penType:  { name: 'pen-type', title: 'pen type' }
     };
 
-    var html = $.mustache($('#livePanelAdjuster').html(), livePanelAdjuster);
+    var templates = [
+        // live panel
+        {
+            name: 'livePanelAdjuster',
+            items: [ 
+                params.capitalHeight,
+                params.xHeight,
+                params.barHeight,
+                params.ascHeight,
+                params.descHeight
+            ],
+            action : function(html) { $('#live-panel').append(html) }
+        },
+        // parameter panel, standard
+        {
+            name: 'parameterPanelAdjusterStandard',
+            items: [ 
+                params.capitalHeight,
+                params.xHeight,
+                params.barHeight,
+                params.ascHeight,
+                params.descHeight
+            ],
+            action: function(html) { $('#parameter-panel').append(html) }
+        },
+        // parameter panel, pen
+        {
+            name: 'parameterPanelAdjusterPen',
+            items: [ params.penType ],
+            action: function(html) { $('#parameter-panel').append(html) }
+        }
+    ]
+
+    $.each(templates, function(i, template) {
+        var html = $.mustache($('#' + template.name).html(), template);
+        template.action(html);
+    });
     
-    $('#live-panel').append(html);
     
-    
-    // kill the spinner
-    $("#loading").spin(false);
+    // stop the spinner
+    $('#loading').spin(false);
+    $('.adjuster').show();
+    $('#parameter-panel').show();
 });

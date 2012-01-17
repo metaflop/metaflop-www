32f4b5301a209e30fbef96ffe3dadb35d582759d
Alexis Reigel <mail@koffeinfrei.org>
2012-01-17 10:24:27 +0100

    updated mf2pt1: optional fontforge settings (scripting)

--- bin/mf2pt1.pl
+++ bin/mf2pt1.pl
@@ -907,7 +907,7 @@ else {
     print FFSCRIPT <<'AUTOHINT';
 Open($1);
 SelectAll();
- Simplify(0, 2);         # Switch these off to get pure font elements (S.Egli)
+# Simplify(0, 2);         # Switch these off to get pure font elements (S.Egli)
  RemoveOverlap();        
  AddExtrema();
  Simplify(0, 2);

ca91c3518fc7f89617761c3338690f149977e11a
koffeinfrei <mail@koffeinfrei.org>
2012-01-26 21:53:26 +0100

    share url within tipsy tooltip

--- public/js/application.js
+++ public/js/application.js
@@ -84,13 +84,15 @@ $(function () {
         var content = previewBox.find('.preview-box-content');
         var previewType = previewBox.attr('id').remove('preview-');
 
-        var url = '/preview/' + previewType + '?' +
-            $.makeArray($('input:text,textarea')).map(function(element){
+        var queryString = $.fn.metaflop.queryString = '?' +
+            $.makeArray($('input:text,textarea').not('[id^=slider-]')).map(function(element){
                 return element.id.remove('param-') + '=' + element.value
             })
             // add the selected character param
             .add('char-number' + '=' + ($('div.char-chooser a.active').attr('href') || '1').remove('#'))
             .join("&");
+
+        var url = '/preview/' + previewType + queryString;
 
         var done = function(error) {
             preloadImage.unbind('load');
@@ -248,6 +250,38 @@ $(function () {
             var value = Number.random(from * 100, to * 100) / 100;
 
             setValue($this, value);
+        });
+
+        return false;
+    });
+
+    // share the current settings
+    $('#action-share-url').click(function(e) {
+        e.preventDefault();
+
+        var link = $(this);
+        var url = "http://www.metaflop.com/" + $.fn.metaflop.queryString.escapeURL();
+        var text = 'I created a nice metaflop font!';
+        var textAndUrl = text + ' ' + url;
+
+        var tipsyContent =
+            '<a href="http://twitter.com/home?status=' + textAndUrl + '" target="_blank" class="action-icon share-twitter" title="post a tweet"><img src="/img/blank.png" /></a>' +
+            '<a href="http://www.facebook.com/sharer.php?u=' + textAndUrl + '" target="_blank" class="action-icon share-facebook" title="post on facebook"><img src="/img/blank.png" /></a>' +
+            '<a href="mailto:?subject=metaflop font&body=' + text + '" target="_blank" class="action-icon share-email" title="send by email"><img src="/img/blank.png" /></a>' +
+            '<span><img src="/img/blank.png" /><object width="16" height="16" id="clippy" class="clippy" classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" title="copy to clipboard"><param value="/flash/clippy.swf" name="movie"><param value="always" name="allowScriptAccess"><param value="high" name="quality"><param value="noscale" name="scale"><param value="text=' + url + '" name="FlashVars"><param value="#FFFFFF" name="bgcolor"><param value="opaque" name="wmode"><embed width="16" height="16" wmode="opaque" bgcolor="#FFFFFF" flashvars="text=' + url + '" pluginspage="http://www.macromedia.com/go/getflashplayer" type="application/x-shockwave-flash" allowscriptaccess="always" quality="high" name="clippy" src="/flash/clippy.swf"></object></span>';
+
+        link.tipsy({
+            trigger: 'manual',
+            fallback: tipsyContent,
+            gravity: 'w',
+            html: true,
+            className: 'tipsy-small'
+        }).tipsy('show');
+
+        // hide tipsy when clicked anywhere outside of it
+        $('body').bind('click.metaflop', function() {
+            link.tipsy('hide');
+            $('body').unbind('click.metaflop');
         });
 
         return false;

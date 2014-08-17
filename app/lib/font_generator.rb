079c9eb16ed3c659508fb62efde322227db0b84e
Alexis Reigel <mail@koffeinfrei.org>
2014-08-17 23:02:02 +0200

    generation of fonts into its own class

---
+++ app/lib/font_generator.rb
@@ -0,0 +1,55 @@
+#
+# metaflop - web interface
+# © 2012 by alexis reigel
+# www.metaflop.com
+#
+# licensed under gpl v3
+#
+
+class FontGenerator
+  def initialize(metaflop)
+    @metaflop = metaflop
+  end
+
+  def otf(preview = false)
+    @metaflop.font_settings.cleanup_tmp_dir
+    # regenerate from the latest parameters with the sidebearings turned off
+    @metaflop.font_parameters.sidebearing.value = '0'
+    @metaflop.font_parameters "#{@metaflop.font_settings.out_dir}/font.mf"
+    @metaflop.font_parameters.to_file(preview)
+
+    out_file = "#{@metaflop.font_settings.out_dir}/font.otf"
+    settings_parameters = @metaflop.font_settings.to_hash
+    settings_parameters[:timeout] = preview ? 5 : 15
+    command = @metaflop.settings[:font_otf] % settings_parameters
+
+    `cd #{@metaflop.font_settings.out_dir} && rm -f #{out_file} && #{command}`
+
+    @metaflop.font_parameters.sidebearing.value = nil
+
+    # if something went wrong (e.g. the timeout got triggered) the
+    # output file does not exist
+    unless File.exist?(out_file)
+      raise Error::Metafont.new
+    end
+
+    { :name => "#{@metaflop.font_settings.font_name}.otf",
+      :data => File.read(out_file) }
+  end
+
+  #  returns base64 encoded otf for embedding as css fontface
+  def preview
+    @metaflop.font_settings.font_hash = 'preview'
+    data = otf(true)[:data]
+    Base64.strict_encode64(data)
+  end
+
+  def web
+    otf
+    `cd #{@metaflop.font_settings.out_dir} && #{@metaflop.settings[:font_web]}`
+
+    font = WebFont.new(@metaflop.font_settings)
+    { :name => "#{font.font_name}_webfont.zip",
+      :data => font.zip }
+  end
+end
--- app/lib/metaflop.rb
+++ app/lib/metaflop.rb
@@ -10,21 +10,23 @@ require './app/lib/rack_logger'
 require './app/lib/rack_settings'
 require './app/lib/font_parameters'
 require './app/lib/font_settings'
+require './app/lib/font_generator'
 require './app/lib/web_font'
 
 class Metaflop
   include RackLogger
   include RackSettings
 
-  # args: see FontParameter
+  # args: see FontParameters / FontSettings
   def initialize(args = {})
     @font_settings = FontSettings.new(args)
     @font_parameters = FontParameters.new(args, @font_settings)
+    @font_generator = FontGenerator.new(self)
   end
 
   def font_parameters(file = nil)
-    if (!@font_parameters_initialized)
-      @font_parameters.from_file file
+    unless @font_parameters_initialized
+      @font_parameters.from_file(file)
       @font_parameters_initialized = true
     end
 
@@ -35,46 +37,16 @@ class Metaflop
     @font_settings
   end
 
-  def font_otf(preview = false)
-    @font_settings.cleanup_tmp_dir
-    # regenerate from the latest parameters with the sidebearings turned off
-    @font_parameters.sidebearing.value = '0'
-    font_parameters "#{@font_settings.out_dir}/font.mf"
-    @font_parameters.to_file(preview)
-
-    out_file = "#{@font_settings.out_dir}/font.otf"
-    settings_parameters = @font_settings.to_hash
-    settings_parameters[:timeout] = preview ? 5 : 15
-    command = settings[:font_otf] % settings_parameters
-
-    `cd #{@font_settings.out_dir} && rm -f #{out_file} && #{command}`
-
-    @font_parameters.sidebearing.value = nil
-
-    # if something went wrong (e.g. the timeout got triggered) the
-    # output file does not exist
-    unless File.exist?(out_file)
-      raise Error::Metafont.new
-    end
-
-    { :name => "#{@font_settings.font_name}.otf",
-      :data => File.read(out_file) }
+  def font_otf(preview)
+    @font_generator.otf(preview)
   end
 
-  #  returns base64 encoded otf for embedding as css fontface
   def font_preview
-    @font_settings.font_hash = 'preview'
-    data = font_otf(true)[:data]
-    Base64.strict_encode64(data)
+    @font_generator.preview
   end
 
   def font_web
-    font_otf
-    `cd #{@font_settings.out_dir} && #{settings[:font_web]}`
-
-    font = WebFont.new(@font_settings)
-    { :name => "#{font.font_name}_webfont.zip",
-      :data => font.zip }
+    @font_generator.web
   end
 
   def self.create(params, settings, logger)

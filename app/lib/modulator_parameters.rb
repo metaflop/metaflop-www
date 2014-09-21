f2e0d47022bf00796c3ad910d685bca840f101fe
Alexis Reigel <mail@koffeinfrei.org>
2014-09-21 20:45:43 +0200

    extract modular parameters to own class

---
+++ app/lib/modulator_parameters.rb
@@ -0,0 +1,106 @@
+#
+# metaflop - web interface
+# © 2012 by alexis reigel
+# www.metaflop.com
+#
+# licensed under gpl v3
+#
+
+class ModulatorParameters
+  def initialize(font_parameters)
+    @font_parameters = font_parameters
+  end
+
+  def default_parameters
+    [{
+      :title => "Dimension",
+      :items => [
+        { :title => 'unit width', :key => :unit_width },
+        { :title => 'pen width', :key => :pen_width },
+        { :title => 'pen height', :key => :pen_height }
+      ]
+    }, {
+      :title => "Proportion",
+      :items => [
+        { :title => 'cap height', :key => :cap_height },
+        { :title => 'bar height', :key => :bar_height },
+        { :title => 'ascender height', :key => :ascender_height },
+        { :title => 'descender height', :key => :descender_height },
+        { :title => 'glyph angle', :key => :glyph_angle },
+        { :title => 'x-height', :key => :x_height },
+        { :title => 'accents height', :key => :accent_height },
+        { :title => 'depth of comma', :key => :comma_depth },
+      ]
+    }, {
+      :title => "Shape",
+      :items => [
+        { :title => 'horizontal increase', :key => :horizontal_increase },
+        { :title => 'vertical increase', :key => :vertical_increase },
+        { :title => 'contrast', :key => :contrast },
+        { :title => 'superness', :key => :superness },
+        { :title => 'pen angle', :key => :pen_angle },
+        { :title => 'pen shape', :key => :pen_shape, :options =>
+          [ { :value => '1', :text => 'Circle' },
+            { :value => '2', :text => 'Square' },
+            { :value => '3', :text => 'Razor' } ] },
+        { :title => 'slanting', :key => :slant },
+        { :title => 'randomness', :key => :craziness }
+      ]
+    }, {
+      :title => "Optical corrections",
+      :items => [
+        { :title => 'apperture', :key => :apperture },
+        { :title => 'corner', :key => :corner },
+        { :title => 'overshoot', :key => :overshoot },
+        { :title => 'taper', :key => :taper },
+      ]
+    }]
+  end
+
+  def all
+    i = 1
+
+    # add properties needed for view
+    all_with_each_item do |item|
+      param = @font_parameters.send(item[:key])
+
+      item[:default] = param.default
+      item[:value] = param.value
+      item[:range_from] = param.range && param.range[:from]
+      item[:range_to] = param.range && param.range[:to]
+      item[:hidden] = param.hidden
+      item[:name] = item[:key]
+      item[:tabindex] = i
+
+      # special case for dropdowns
+      if item[:options]
+        item[:dropdown] = true
+        selected = item[:options].select { |option| option[:value] == param.value }.first
+        if selected
+          selected[:selected] = true
+        end
+      end
+
+      i = i + 1
+    end
+  end
+
+  def all_with_each_item
+    groups = default_parameters
+
+    groups.each do |group|
+      group[:items].each do |item|
+        yield item
+      end
+
+      # remove non-mapped params
+      group[:items].reject! { |item| !item[:default] || item[:hidden] }
+    end
+
+    # remove empty groups
+    groups.reject! { |group| group[:items].empty? }
+
+    groups
+  end
+end
+
--- app/views/modulator.rb
+++ app/views/modulator.rb
@@ -6,87 +6,15 @@
 # licensed under gpl v3
 #
 
+require './app/lib/modulator_parameters'
+
 class App
   module Views
     class Modulator < Layout
       include LogicLessSlim
 
       def parameters
-        groups = [{
-          :title => "Dimension",
-          :items => [
-            { :title => 'unit width', :key => :unit_width },
-            { :title => 'pen width', :key => :pen_width },
-            { :title => 'pen height', :key => :pen_height }
-          ]
-        }, {
-          :title => "Proportion",
-          :items => [
-            { :title => 'cap height', :key => :cap_height },
-            { :title => 'bar height', :key => :bar_height },
-            { :title => 'ascender height', :key => :ascender_height },
-            { :title => 'descender height', :key => :descender_height },
-            { :title => 'glyph angle', :key => :glyph_angle },
-            { :title => 'x-height', :key => :x_height },
-            { :title => 'accents height', :key => :accent_height },
-            { :title => 'depth of comma', :key => :comma_depth },
-          ]
-        }, {
-          :title => "Shape",
-          :items => [
-            { :title => 'horizontal increase', :key => :horizontal_increase },
-            { :title => 'vertical increase', :key => :vertical_increase },
-            { :title => 'contrast', :key => :contrast },
-            { :title => 'superness', :key => :superness },
-            { :title => 'pen angle', :key => :pen_angle },
-            { :title => 'pen shape', :key => :pen_shape, :options =>
-              [ { :value => '1', :text => 'Circle' },
-                { :value => '2', :text => 'Square' },
-                { :value => '3', :text => 'Razor' } ] },
-            { :title => 'slanting', :key => :slant },
-            { :title => 'randomness', :key => :craziness }
-          ]
-        }, {
-          :title => "Optical corrections",
-          :items => [
-            { :title => 'apperture', :key => :apperture },
-            { :title => 'corner', :key => :corner },
-            { :title => 'overshoot', :key => :overshoot },
-            { :title => 'taper', :key => :taper },
-          ]
-        }]
-
-        # add properties needed for view
-        i = 1
-        groups.each do |group|
-          group[:items].each do |x|
-            param = @font_parameters.send(x[:key])
-            x[:default] = param.default
-            x[:value] = param.value
-            x[:range_from] = param.range.nil? ? nil : param.range[:from]
-            x[:range_to] = param.range.nil? ? nil : param.range[:to]
-            x[:hidden] = param.hidden
-            x[:name] = x[:key]
-            x[:tabindex] = i
-
-            # special case for dropdowns
-            if x[:options]
-              x[:dropdown] = true
-              selected = x[:options].select { |option| option[:value] == param.value }.first
-              if selected
-                selected[:selected] = true
-              end
-            end
-
-            i = i + 1
-          end
-          .keep_if { |x| x[:default] && !x[:hidden] } # remove non-mapped params
-        end
-
-        # remove empty groups
-        groups.delete_if { |x| x[:items].empty? }
-
-        groups
+        ModulatorParameters.new(@font_parameters).all
       end
 
       # single preview char chooser

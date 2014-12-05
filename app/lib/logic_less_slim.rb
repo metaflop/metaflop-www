f2700c036e58d50305fb9f10cc9e9e88461ef55d
Alexis Reigel <mail@koffeinfrei.org>
2014-12-05 23:28:46 +0100

    split #slim to smaller methods

--- app/lib/logic_less_slim.rb
+++ app/lib/logic_less_slim.rb
@@ -19,41 +19,69 @@ module LogicLessSlim
 
   # set the corresponding view model class to the slim view
   def slim(template, layout: true, http_caching: true)
+    load_template(template)
+
+    set_view_model(template)
+    template = get_template(template)
+
+    assign_instance_variables_to_view
+
+    render_template(template, layout, http_caching)
+  end
+
+  private
+
+  def load_template(template)
     begin
       require "./app/views/#{template}"
     rescue LoadError
       raise Metaflop::Error::TemplateNotFound.new
     end
+  end
 
+  def set_view_model(template)
     @view_model = "App::Views::#{template.to_s.camelize}".constantize.new
+  end
 
-    # custom template set on class
-    unless @view_model.class.template.nil?
-      template = @view_model.class.template
-    end
+  def get_template(template)
+    # get the custom template set on class
+    @view_model.class.template || template
+  end
 
+  def assign_instance_variables_to_view
     # copy instance variables set in sinatra to the view
     instance_variables.each do |name|
       @view_model.instance_variable_set(name, instance_variable_get(name))
     end
+  end
 
+  def render_template(template, layout, http_caching)
     options = { layout: layout, dictionary: 'self.view_model' }
 
     if respond_to? :render, true
-      # `render` needs a symbol, but it's safe here
-      # as above the check returns if an invalid template
-      # name is provided
-      content = render :slim, template.to_sym, options
-
-      if http_caching
-        set_http_cache(content)
-      end
-
-      content
+      render_native(template, options, http_caching)
     else
-      require 'slim'
-      require 'tilt'
-      Tilt.new("./app/views/#{template}.slim", options).render(self)
+      render_tilt(template, options)
     end
+  end
+
+  def render_native(template, options, http_caching)
+    # `render` needs a symbol, but it's safe here
+    # (no symbol dos) as above the check
+    # (see `#load_template`) returns if an invalid
+    # template name is provided
+    content = render :slim, template.to_sym, options
+
+    if http_caching
+      set_http_cache(content)
+    end
+
+    content
+  end
+
+  def render_tilt(template, options)
+    require 'slim'
+    require 'tilt'
+    Tilt.new("./app/views/#{template}.slim", options).render(self)
   end
 end

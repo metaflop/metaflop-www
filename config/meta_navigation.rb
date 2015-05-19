9e2d1b65f698720c673a1c1489893e494536ee43
Alexis Reigel <mail@koffeinfrei.org>
2015-05-19 18:50:22 +0200

    unify social media link titles

--- config/meta_navigation.rb
+++ config/meta_navigation.rb
@@ -2,15 +2,15 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
     primary.item :twitter, '<i class="fa fa-twitter"></i>',
       'http://twitter.com/metaflop', link_html: {
-        target: '_blank', class: 'tooltip', title: 'follow us on twitter'
+        target: '_blank', class: 'tooltip', title: 'visit us on twitter'
       }
     primary.item :facebook, '<i class="fa fa-facebook"></i>',
       'http://www.facebook.com/metaflop', link_html: {
-        target: '_blank', class: 'tooltip', title: 'like us on facebook'
+        target: '_blank', class: 'tooltip', title: 'visit us on facebook'
       }
     primary.item :github, '<i class="fa fa-github"></i>',
       'https://github.com/metaflop', link_html: {
-        target: '_blank', class: 'tooltip', title: 'get the source code on github'
+        target: '_blank', class: 'tooltip', title: 'visit us on github'
       }
     primary.item :'donate-paypal', '<i class="fa fa-dollar"></i>',
       '#', link_html: {

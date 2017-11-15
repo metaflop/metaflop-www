4c008f627def3b63be658f1830e680c67590083e
Alexis Reigel <mail@koffeinfrei.org>
2017-11-15 20:22:32 +0100

    tmp dir cleanup cronjob -> don't bail on non-dir

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,4 +1,4 @@
 every 1.hours do
   # remove tmp dirs that haven't been used in the last hour
-  command "find /tmp/metaflop -maxdepth 1 -type d -mtime 0.042 -exec rm -r '{}' \\;"
+  command "[ -d /tmp/metaflop ] && find /tmp/metaflop -maxdepth 1 -type d -mtime 0.042 -exec rm -r '{}' \\;"
 end
